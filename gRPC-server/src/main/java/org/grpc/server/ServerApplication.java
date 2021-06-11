da86bdb03a6bfab057261164d3d5f314bd4a5eed
rabbani <bk.rabbani@gmail.com>
2021-06-11 20:14:31 +0530

    Created spring boot skeleton

---
+++ gRPC-server/src/main/java/org/grpc/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package org.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
